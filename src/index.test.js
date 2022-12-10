6ca04e3285a6b6593b4ee666e0209a2f7c4008d5
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-10 12:22:47 +0000

    TDD kata for fizzbuzz

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,30 @@
-import {projectName} from "./index";
+const fizzBuzz = require('./fizzbuzz');
 
-describe('Starter project should', () => {
-  it('output its name', () => {
-    expect(projectName()).toEqual('Kata starter project');
-  });
+describe('fizzBuzz', () => {
+
+    it('returns Fizzbuzz if divisble by 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    });
+
+    it('returns Fizz if divisble by 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz');
+    });
+
+    it('returns Buzz if divisble by 5', () => {
+        expect(fizzBuzz(5)).toBe('Buzz');
+    });
+
+    it('returns 8 if divisble by 8', () => {
+        expect(fizzBuzz(8)).toBe(8);
+    });
+
 });
+
+
+// import {projectName} from "./index";
+
+// describe('Starter project should', () => {
+//   it('output its name', () => {
+//     expect(projectName()).toEqual('Kata starter project');
+//   });
+// });
